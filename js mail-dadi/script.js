fd2088124825679fb307d614d9a4b77d9423399c
ilariamotta <ilaria.motta93@gmail.com>
2025-11-02 17:08:26 +0100

    feat: creazione array esercizio mail

---
+++ js mail-dadi/script.js
@@ -0,0 +1,17 @@
+// MAIL
+// Crea un array con delle email di chi può acedere.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email.
+// NON USARE INCLUDES!
+
+//1. creo un array con delle mail 
+//2. con prompt chiedo la mail utente
+//3. confronto mail immessa con quelle presenti nell'array
+//4. resistuisco messaggio 
+
+
+const mail = ["silvanamail", "harrymail", "tommasomail", "caterinamail"];
+
+console.log(mail)
