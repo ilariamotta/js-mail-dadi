76a29b645018cf2ce65eed990bbaa2b70b0f961d
ilariamotta <ilaria.motta93@gmail.com>
2025-11-02 17:15:11 +0100

    feat: logica esercizio dadi

--- js mail-dadi/script.js
+++ js mail-dadi/script.js
@@ -20,3 +20,14 @@ if (prompt === mail) {
 }
 
 console.log("Mail valida")
+
+//////////////////////////////////////////
+// GIOCO DEI DADI
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//1. creo variabile vuota numero giocatore & creo variabile vuota numero computer
+//2. genero un numero random sia per il giocatore che per il computer, salvo i numeri
+//3. confronto i due numeri
+    // SE numero giocatore è più alto di computer ALLORA ha vinto giocatore, stampo messaggio vittoria
+    // ALTRIMENTI numero giocatore è più basso di computer ALLORA ha vinto computer, stampo messaggio
